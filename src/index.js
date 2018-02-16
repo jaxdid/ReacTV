442143372a2c90d105d5c97c7e41c485eec57c25
Alain Lemaire <jaxdid@gmail.com>
2018-02-16 20:25:54 +0000

    Add Serch Bar component and YouTube API key

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import SearchBar from './components/SearchBar'
+const { youTubeApiKey } = require('../secrets')
 
 const App = () => {
-  return <div>Hello </div>
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  )
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
