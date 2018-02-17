dbbd8d592ca241b93a80b85ec39d63a32c8fa61a
Alain Lemaire <jaxdid@gmail.com>
2018-02-17 12:44:36 +0000

    Styles video list and displays video titles, thumbnails

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const VideoListItem = ({ video }) => {
-  return <li>{video.etag}</li>
+  const { thumbnails, title } = video.snippet
+  
+  return (
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className="media-left">
+          <img className="media-object" src={thumbnails.default.url} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{title}</div>
+        </div>
+      </div>
+    </li>
+  )
 }
 
-export default VideoListItem
+export default VideoListItem 
