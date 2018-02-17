b1d060841070b6b972a9b5337d210b186fbed5e0
Alain Lemaire <jaxdid@gmail.com>
2018-02-17 12:18:02 +0000

    Builds out video list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import VideoListItem from './VideoListItem'
 
 const VideoList = ({ videos }) => {
+  const videoListItems = videos.map(video => {
+    return <VideoListItem key={video.etag} video={video} />
+  })
+
   return (
     <ul className="col-md-4 list-group">
-      {videos.length}
+      {videoListItems}
     </ul>
   )
 }
---
+++ src/components/VideoListItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const VideoListItem = ({ video }) => {
+  return <li>{video.etag}</li>
+}
+
+export default VideoListItem
